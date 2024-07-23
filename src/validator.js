5749330599b70b8bf05d31289669546ce6c116b4
Calico Seders <calico.seders@seiu.org>
2024-07-22 17:33:32 -0700

    validator file with some comments

---
+++ src/validator.js
@@ -0,0 +1,11 @@
+// This validator expects First Name, Last Name, Id #, Employer, Job status, Dues, COPE
+
+// Things that can be blank: Dues, COPE
+
+// 40 char limit on first name, 50 char limit on last name
+
+// Dues, COPE, should be "moneyish"
+// strip $
+// use Mike's regex to check to be numeric
+
+// Job status value should match one of the values in an array
