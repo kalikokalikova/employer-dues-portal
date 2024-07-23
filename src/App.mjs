3f193efd254cc1c91191733c9a0831945b46fe3d
lilylovesyou <62107222+lilylovesyou@users.noreply.github.com>
2024-07-22 20:23:46 -0400

    Removed Result and ResultSet from project.

--- src/App.mjs
+++ src/App.mjs
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import SelectDataset from "./SelectDataset.mjs";
 import TablePreview from "./TablePreview.mjs";
-import ResultSet from "./ResultSet.mjs";
 
 export default function App() {
     const [dataset, setDataset] = useState(null);
@@ -16,6 +15,5 @@ export default function App() {
         <p>A program that accepts a spreadsheet file, then produces separate output files segmented by a column in the data. Current implementation is csv only.</p>
         <SelectDataset dataset={dataset} setDataset={setDatasetSafe} />
         {dataset !== null && segmentIndex === null ? <TablePreview dataset={dataset} setSegmentIndex={setSegmentIndex} /> : null}
-        {dataset !== null && segmentIndex !== null ? <ResultSet dataset={dataset} segmentIndex={segmentIndex} setSegmentIndex={setSegmentIndex} /> : null}
     </>;
 }
