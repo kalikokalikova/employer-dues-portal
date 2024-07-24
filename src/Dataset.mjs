990b0aa2607149c0e4c0328054445686e40c5802
lilylovesyou <62107222+lilylovesyou@users.noreply.github.com>
2024-07-24 12:10:23 -0400

    Tweaked Dataset to have a new export method.

--- src/Dataset.mjs
+++ src/Dataset.mjs
@@ -2,8 +2,8 @@ import csv from "papaparse";
 
 export default class Dataset {
   file;
-  rawdata;
-  printabledata;
+  originaldata;
+  modifieddata;
   csvmetadata;
 
   constructor(file) {
@@ -18,8 +18,8 @@ export default class Dataset {
     return new Promise((resolve, reject) => {
       csv.parse(this.file, {
         complete: (results) => {
-          this.rawdata = results.data;
-          this.printabledata = results.data;
+          this.originaldata = results.data;
+          this.modifieddata = results.data;
           this.csvmetadata = {
             delimiter: results.meta["delimiter"],
             linebreak: results.meta["linebreak"],
@@ -31,35 +31,13 @@ export default class Dataset {
     });
   }
 
-  segmentCSV(coordinateIndex) {
-    let headers = this.rawdata.slice(0, coordinateIndex[0] + 1);
-    let body = this.rawdata.slice(coordinateIndex[0] + 1);
-
-    let segments = {};
-    body.forEach((row) => {
-      let indexvalue = row[coordinateIndex[1]];
-      if (indexvalue in segments) {
-        segments[indexvalue].push(row);
-      } else {
-        segments[indexvalue] = [row];
-      }
-    });
-
+  exportCSVFile() {
+    //Split original filename into name and extension parts
     let match = /^(.+)(\.\w+)$/g.exec(this.file.name);
-    let output = [];
-    for (let key in segments) {
-      let contents = headers.concat(segments[key]);
-      output.push({
-        segment: key,
-        headerrows: headers.length,
-        bodyrows: segments[key].length,
-        file: new File(
-          [csv.unparse(contents, this.csvmetadata)],
-          match[1] + ` - ${key.replace(/[^0-9a-zA-Z-\._ ]/g, "")}` + match[2],
-          { type: this.file.type }
-        ),
-      });
-    }
-    return output;
+    return new File(
+      [csv.unparse(this.modifieddata, this.csvmetadata)],
+      `${match[1]} - validated${match[2]}`, //Generate a new filename
+      { type: this.file.type }
+    );
   }
 }
